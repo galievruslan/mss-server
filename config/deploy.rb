e3c4551a27fd0aa09aeaab30e412b5d2bdc0933b
Ruslan Galiev <galievruslan@gmail.com>
2013-03-28 09:45:25 +0400

    fix deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,29 +1,29 @@
-require "rvm/capistrano"
-require 'bundler/capistrano'
+# APPLICATION DETAILS #######################################
+set :application, "mss"
+set :deploy_to, "/var/www/#{application}"
+set :rails_env, "production"
 
-ssh_options[:forward_agent] = true
-set :application, 'mss-server'
-set :rvm_ruby_string, '1.9.3'
-set :repository,  "git@github.com:galievruslan/mss-server.git" 
-set :deploy_to, "/team/mss" 
-set :deploy_via, :remote_cache
-set :scm, 'git'
-set :branch, 'master'
-set :scm_verbose, true
+# GETTING INTO THE PRODUCTION SERVER ########################
+set :user, "deploy"
 set :use_sudo, false
-set :rails_env, :production
-set :default_stage, 'production'
-set :normalize_asset_timestamps, false
+ssh_options[:paranoid] = false 
+set :domain, "192.168.3.109"
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
 
+# GIT AND GITHUB ############################################
+set :scm, :git
+set :repository,  "git@github.com:galievruslan/mss-server.git"
+set :branch, "master"
+set :scm_command, "/usr/bin/git"
+
+after "deploy:update_code","deploy:restart"
+
+# TASKS #####################################################
 namespace :deploy do
-  desc "cause Passenger to initiate a restart"
+  desc "Restarting the app"  
   task :restart do
-    run "touch #{current_path}/tmp/restart.txt" 
-  end
-  desc "reload the database with seed data"
-  task :seed do
-    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+    run "touch #{shared_path}/tmp/restart.txt"
   end
 end
-
-server '192.168.3.109', :web, :app, :db, :primary => true
