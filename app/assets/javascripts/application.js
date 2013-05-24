85f9cbb28a251890aa029a064f2afe3b391f83d1
Ruslan Galiev <galievruslan@gmail.com>
2013-05-24 11:39:39 +0400

    Remove neste_form js link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
-//= require jquery_nested_form
 //= require cocoon
 //= require twitter/bootstrap
 //= require bootstrap-datetimepicker
