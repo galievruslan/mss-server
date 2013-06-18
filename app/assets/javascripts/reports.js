a0c1ab3199f13f499489064734d95691ef395dc0
Ruslan Galiev <galievruslan@gmail.com>
2013-06-18 16:03:14 +0400

    Add miltiple setect in reports view

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-	$("#q_shipping_address_customer_id_eq").select2({minimumInputLength: 2, allowClear: true});
-	$("#q_shipping_address_id_eq").select2({minimumInputLength: 2, allowClear: true});
-	$("#q_manager_id_eq").select2({allowClear: true});
-	$("#q_warehouse_id_eq").select2({allowClear: true});
-	$("#q_price_list_id_eq").select2({allowClear: true});
-	$("#q_route_manager_id_eq").select2({allowClear: true});
-	$("#q_status_id_eq").select2({allowClear: true});
-	$("#q_template_route_day_of_week_eq").select2({allowClear: true});
-	$("#q_template_route_manager_id_eq").select2({allowClear: true});
+	$("#q_shipping_address_customer_id_in").select2({minimumInputLength: 2, allowClear: true});
+	$("#q_shipping_address_id_in").select2({minimumInputLength: 2, allowClear: true});
+	$("#q_manager_id_in").select2({allowClear: true});
+	$("#q_warehouse_id_in").select2({allowClear: true});
+	$("#q_price_list_id_in").select2({allowClear: true});
+	$("#q_route_manager_id_in").select2({allowClear: true});
+	$("#q_status_id_in").select2({allowClear: true});
+	$("#q_template_route_day_of_week_in").select2({allowClear: true});
+	$("#q_template_route_manager_id_in").select2({allowClear: true});
 });
