9beecab8bd3d368a251f7eb9c17e9344bf046589
Ruslan Galiev <galievruslan@gmail.com>
2013-06-24 10:40:14 +0400

    Change bootstrap flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::Base
   
   
   rescue_from CanCan::AccessDenied do |exception|
-    flash[:error] = exception.message
     redirect_to root_url, :alert => exception.message
   end
   
