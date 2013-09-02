b33ea35a8acd0552cf206ff3a2aaa389500e34a9
Ruslan Galiev <galievruslan@gmail.com>
2013-09-02 08:26:08 +0400

    Fix exchange bug

--- app/controllers/exchange_controller.rb
+++ app/controllers/exchange_controller.rb
@@ -226,8 +226,10 @@ class ExchangeController < ApplicationController
             manager_id = Manager.find_by_external_key(manager_external_key).id
             shipping_address_id = ShippingAddress.find_by_external_key(shipping_address_external_key).id
             manager_shipping_addresses_true = ManagerShippingAddress.find_by_manager_id_and_shipping_address_id(manager_id, shipping_address_id)
-            manager_shipping_addresses_true_id = manager_shipping_addresses_true.id unless manager_shipping_addresses_true.nil?
-            manager_shipping_addresses_true_ids << manager_shipping_addresses_true_id
+            unless manager_shipping_addresses_true.nil?
+              manager_shipping_addresses_true_id = manager_shipping_addresses_true.id 
+              manager_shipping_addresses_true_ids << manager_shipping_addresses_true_id
+            end            
           end          
           
           manager_shipping_addresses_false = ManagerShippingAddress.where("id NOT IN (?)", manager_shipping_addresses_true_ids)
@@ -451,8 +453,10 @@ class ExchangeController < ApplicationController
             product_id = Product.find_by_external_key(product_external_key).id
             unit_of_measure_id = UnitOfMeasure.find_by_external_key(unit_of_measure_external_key).id
             product_unit_of_measures_true = ProductUnitOfMeasure.find_by_product_id_and_unit_of_measure_id(product_id, unit_of_measure_id)
-            product_unit_of_measures_true_id = product_unit_of_measures_true.id unless product_unit_of_measures_true.nil?
-            product_unit_of_measures_true_ids << product_unit_of_measures_true_id
+            unless product_unit_of_measures_true.nil?
+              product_unit_of_measures_true_id = product_unit_of_measures_true.id 
+              product_unit_of_measures_true_ids << product_unit_of_measures_true_id
+            end
           end
           product_unit_of_measures_false = ProductUnitOfMeasure.where("id NOT IN (?)", product_unit_of_measures_true_ids)
           product_unit_of_measures_false.each do |product_unit_of_measure_false|
@@ -510,8 +514,10 @@ class ExchangeController < ApplicationController
             product_id = Product.find_by_external_key(product_external_key).id
             price_list_id = PriceList.find_by_external_key(price_list_external_key).id
             product_prices_true = ProductPrice.find_by_product_id_and_price_list_id(product_id, price_list_id)
-            product_prices_true_id = product_prices_true.id unless product_prices_true.nil?
-            product_prices_true_ids << product_prices_true_id
+            unless product_prices_true.nil?
+              product_prices_true_id = product_prices_true.id 
+              product_prices_true_ids << product_prices_true_id
+            end
           end
           
           product_prices_false = ProductPrice.where("id NOT IN (?)", product_prices_true_ids)
